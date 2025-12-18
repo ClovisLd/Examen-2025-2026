211aee11f46b493c14b7df6d6024ee0917693b41
ClovisLd <clovis.leden@gmail.com>
2025-12-18 10:55:29 +0100

    a

--- examen-2025-2026/app/bookings/[id]/page.tsx
+++ examen-2025-2026/app/bookings/[id]/page.tsx
@@ -1,13 +1,8 @@
-"use server";
-import { useParams } from "next/navigation";
-
-export default function DynamicRoute() {
-  const params = useParams();
-
-  return (
-    <div>
-      <h1>GeeksforGeeks</h1>
-      <h2>Dynamic Parameter: {params.data}</h2>
-    </div>
-  );
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  return <div>My Post: {slug}</div>;
 }
