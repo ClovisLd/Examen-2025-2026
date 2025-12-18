d8ca5bdfd39f341ad24aafbd07fa14d36cf1043f
ClovisLd <clovis.leden@gmail.com>
2025-12-18 12:00:18 +0100

    a

--- examen-2025-2026/app/bookings/[id]/page.tsx
+++ examen-2025-2026/app/bookings/[id]/page.tsx
@@ -1,8 +1,9 @@
-export default async function Page({
-  params,
-}: {
+interface PageProps {
   params: Promise<{ slug: string }>;
-}) {
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Page({ params }: PageProps) {
   const { slug } = await params;
   return <div>My Post: {slug}</div>;
 }
